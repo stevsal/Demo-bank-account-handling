6af6a27c4bd3a05f3ffedca51d6d63663164232e
Steven Saluri <43164747+stevsal@users.noreply.github.com>
2022-05-10 15:58:53 +0300

    Initial commit

---
+++ src/main/java/com/example/Demo/bank/account/handling/DemoBankAccountHandlingApplication.java
@@ -0,0 +1,14 @@
+package com.example.Demo.bank.account.handling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBankAccountHandlingApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoBankAccountHandlingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Demo/bank/account/handling/DemoBankAccountHandlingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demo.bank.account.handling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBankAccountHandlingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
